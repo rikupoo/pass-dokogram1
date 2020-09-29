ef13ba8106e557e541a6d45f9cedd5dadd803239
rikupoo <nemutir@gmail.com>
2020-09-29 18:26:16 +0900

    メッセージ投稿機能

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,6 +3,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @group = Group.find(params[:group_id])
+    @messages = @group.messages.includes(:user)
   end
 
   def create
@@ -11,6 +12,7 @@ class MessagesController < ApplicationController
     if @message.save
       redirect_to group_messages_path(@group)
     else
+      @messages = @group.messages.includes(:user)
       render 'index'
     end
   end
