58903dab0b374eba9ea405ae4974a28c924ea286
rikupoo <nemutir@gmail.com>
2020-09-29 15:20:28 +0900

    新規グループ作成

---
+++ app/controllers/groups_controller.rb
@@ -0,0 +1,21 @@
+class GroupsController < ApplicationController
+  def new
+    @group = Group.new
+  end
+
+  def create
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def group_params
+    params.require(:group).permit(:name).merge(user_id:current_user.id)
+  end
+
+end
